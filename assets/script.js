1e1e86cff79f5197c11ad8b106c51c63ceb9f1f5
Corrinne W <corrinneworden@gmail.com>
2021-03-01 16:59:57 -0500

    added notes to HTML and created reference variables in JS

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,6 @@
-script.js
+//reference each HTML element
+const searchBar = $('#searchBar');
+const searchButton = $('#searchButton');
+const searchHistory = $('#searchHistory');
+const currentTemp = $('#currentTemp');
+const forecastContainer = $('#forecastContainer');
