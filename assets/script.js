c2ae7d01d1f0ff9a7c19d31a992e7070485b68d3
Corrinne W <corrinneworden@gmail.com>
2021-03-02 11:54:07 -0500

    set up search button to return open weather API data for user input city name

--- assets/script.js
+++ assets/script.js
@@ -6,24 +6,39 @@ const currentWeather = $('#currentWeather');
 const forecastContainer = $('#forecastContainer');
 
 //global variables
-let cityName = 'Columbus';
+let cityName;
 
-const searchURL = 'http://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=26296fad5e8eec50db14b2b3a9c853be';
+let currentURL;
 
+let forecastURL;
 //search bar
-cityName = $(searchBar).val()
 
 //search button
 //api.openweathermap.org/data/2.5/forecast?q={city name},{state code}&appid={26296fad5e8eec50db14b2b3a9c853be}
-fetch(searchURL, {
-  cache: "reload"
-})
-  .then(function (response) {
-    return response.json();
+searchButton.click(function () {
+  cityName = searchBar.val();
+  currentURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=26296fad5e8eec50db14b2b3a9c853be';
+  forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&units=imperial&appid=26296fad5e8eec50db14b2b3a9c853be';
+  fetch(currentURL, {
+    cache: "reload"
   })
-  .then(function (data) {
-    console.log(data);
-  });
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+  
+  fetch(forecastURL, {
+    cache: "reload"
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });  
+});
 
 //search history buttons
 
