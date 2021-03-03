7844f67d05df1c073222edec48cbeb8e6d8d06b5
Corrinne W <corrinneworden@gmail.com>
2021-03-03 10:33:45 -0500

    set up array to save user searches to localStorage and created a set from that array to filter out duplicate searches

--- assets/script.js
+++ assets/script.js
@@ -11,14 +11,23 @@ let cityName;
 let currentURL;
 
 let forecastURL;
-//search bar
 
-//search button
-//api.openweathermap.org/data/2.5/forecast?q={city name},{state code}&appid={26296fad5e8eec50db14b2b3a9c853be}
+let history =  JSON.parse(localStorage.getItem('cityName')) || [];
+
+//filters duplicates from history
+let uniqueHistory = [...new Set(history)];
+
+//search bar and button
 searchButton.click(function () {
   cityName = searchBar.val();
+  //push current cityName to cityArray in local storage
+  history.push(cityName);
+  localStorage.setItem('cityName', JSON.stringify(history))
+
+  //write URL based on user-input city name
   currentURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=26296fad5e8eec50db14b2b3a9c853be';
   forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&units=imperial&appid=26296fad5e8eec50db14b2b3a9c853be';
+  
   fetch(currentURL, {
     cache: "reload"
   })
@@ -40,7 +49,8 @@ searchButton.click(function () {
     });  
 });
 
-//search history buttons
+//create search history buttons
+
 
 //Current weather
 
