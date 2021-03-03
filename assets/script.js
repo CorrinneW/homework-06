80e444040b1cd3f9b726d0f195268114506d281a
Corrinne W <corrinneworden@gmail.com>
2021-03-03 14:02:52 -0500

    added geolocation api to convert city name to geographic coordinates

--- assets/script.js
+++ assets/script.js
@@ -6,48 +6,41 @@ const currentWeather = $('#currentWeather');
 const forecastContainer = $('#forecastContainer');
 
 //global variables
-let cityName;
+let inputCity;
+let geolocation;
+let lat;
+let long;
 
-let currentURL;
 
-let forecastURL;
-
-let history =  JSON.parse(localStorage.getItem('cityName')) || [];
+let history =  JSON.parse(localStorage.getItem('inputCity')) || [];
 
 //filters duplicates from history
 let uniqueHistory = [...new Set(history)];
 
 //search bar and button
 searchButton.click(function () {
-  cityName = searchBar.val();
-  //push current cityName to cityArray in local storage
-  history.push(cityName);
-  localStorage.setItem('cityName', JSON.stringify(history))
+  inputCity = searchBar.val()
+  //push current inputCity to cityArray in local storage
+  history.push(inputCity);
+  localStorage.setItem('inputCity', JSON.stringify(history));
 
-  //write URL based on user-input city name
-  currentURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=26296fad5e8eec50db14b2b3a9c853be';
-  forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&units=imperial&appid=26296fad5e8eec50db14b2b3a9c853be';
+  //convert inputCity to geographic coordinates
+  geolocation = 'https://api.openweathermap.org/geo/1.0/direct?q='+ inputCity + '&appid=26296fad5e8eec50db14b2b3a9c853be';
   
-  fetch(currentURL, {
-    cache: "reload"
-  })
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
+  fetch(geolocation)
+    .then(response => response.json())
+    .then(data => {
+      let last =data[data.length-1];
+      lat = last.lat;
+      lon = last.lon;
       console.log(data);
+      console.log(lat, lon);
     });
-  
-  fetch(forecastURL, {
-    cache: "reload"
-  })
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-    });  
 });
+
+function fetchApi(inputCity) {
+  
+}
 
 //create search history buttons
 
